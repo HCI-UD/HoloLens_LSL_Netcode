9b6572de12e1211c00f8e9c472f52507ff6834a1
Ryan Bilash <rbilash@udel.edu>
2023-03-01 16:12:51 -0500

    gaze cursor 2

---
+++ Assets/GazeCursor.cs
@@ -0,0 +1,36 @@
+using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Input;
+using UnityEngine;
+
+public class GazeCursor : MonoBehaviour
+{
+    private MeshRenderer meshRenderer;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Grab the mesh renderer that's on the same object as this script.
+        meshRenderer = this.gameObject.GetComponent<MeshRenderer>();
+        PointerUtils.SetGazePointerBehavior(PointerBehavior.AlwaysOn);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Do a raycast into the world based on the user's
+        // head position and orientation.
+        if (CoreServices.InputSystem.EyeGazeProvider.IsEyeTrackingEnabledAndValid)
+        {
+            //Debug.Log("enabled");
+            gameObject.transform.position = CoreServices.InputSystem.EyeGazeProvider.HitPosition;
+        }
+        else
+        {
+            Debug.Log("not enabled");
+        }
+        //Debug.Log(CoreServices.InputSystem.EyeGazeProvider.GazeTarget);
+
+        
+    }
+
+}
