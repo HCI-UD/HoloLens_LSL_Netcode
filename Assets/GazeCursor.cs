cb916628a028b09442523f0cc3840a5a1d9db802
laurenbaron <lbaron@udel.edu>
2023-03-16 13:30:24 -0400

    rotation of headset is streaming

--- Assets/GazeCursor.cs
+++ Assets/GazeCursor.cs
@@ -2,46 +2,42 @@ using Microsoft.MixedReality.Toolkit;
 using Microsoft.MixedReality.Toolkit.Input;
 using UnityEngine;
 
+/// <summary>
+/// tracks the user's eye gaze by mirroring the purple sphere's location to where they are looking 
+/// </summary>
 public class GazeCursor : MonoBehaviour
 {
-    //private MeshRenderer meshRenderer;
-
     // Use this for initialization
     void Start()
     {
-        // Grab the mesh renderer that's on the same object as this script.
-        //meshRenderer = this.gameObject.GetComponent<MeshRenderer>();
         PointerUtils.SetGazePointerBehavior(PointerBehavior.AlwaysOn);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log("test : " + CoreServices.InputSystem.EyeGazeProvider);
-       // Debug.Log("test IsEyeTrackingEnabled: "+ CoreServices.InputSystem.EyeGazeProvider.IsEyeTrackingEnabled);
-      //  Debug.Log("test IsEyeCalibrationValid: " + CoreServices.InputSystem.EyeGazeProvider.IsEyeCalibrationValid);
-       // Debug.Log("test IsEyeTrackingEnabledAndValid: " + CoreServices.InputSystem.EyeGazeProvider.IsEyeTrackingEnabledAndValid);
-      //  Debug.Log("test IsEyeTrackingDataValid: " + CoreServices.InputSystem.EyeGazeProvider.IsEyeTrackingDataValid);
-       
-
-        // Do a raycast into the world based on the user's
-        // head position and orientation.
+        // make sure eye tracking is enabled first
+        // (if not, check configurations in unity: https://learn.microsoft.com/en-us/windows/mixed-reality/mrtk-unity/mrtk2/features/input/eye-tracking/eye-tracking-basic-setup?view=mrtkunity-2022-05#a-note-on-the-gazeinput-capability
+        // and configure setting in HoloLens: Settings->Calibration->Run Eye Calibration)
         if (CoreServices.InputSystem.EyeGazeProvider.IsEyeTrackingEnabledAndValid)
         {
-            // Debug.Log("enabled");
+
             // Show the object at the hit position of the user's eye gaze ray with a target.
             // We want the position always, not just when they hit a target object with gaze.
-            if (CoreServices.InputSystem.EyeGazeProvider.GazeTarget) {
+
+            if (CoreServices.InputSystem.EyeGazeProvider.GazeTarget) {  // if user is looking at an object, the gaze is the hit on that target
+
                 gameObject.transform.position = CoreServices.InputSystem.EyeGazeProvider.HitPosition;
-                //reset target hit info so gaze doesn't get stuck on the target when not looking at it
+
+                //reset target hit info so gaze doesn't get stuck on the last target when user is not looking at it anymore
                 CoreServices.InputSystem.EyeGazeProvider.UpdateGazeInfoFromHit(new MixedRealityRaycastHit());
-            } else
+
+            } else //otherwise if there is not a target the user is looking at then the gaze is the direction the user is looking
             {
+
                 gameObject.transform.position = CoreServices.InputSystem.EyeGazeProvider.GazeDirection;
                 CoreServices.InputSystem.EyeGazeProvider.UpdateGazeInfoFromHit(new MixedRealityRaycastHit());
             }
-
-            Debug.Log(CoreServices.InputSystem.EyeGazeProvider.GazeDirection);
         }
         else
         {
